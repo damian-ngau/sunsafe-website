3e1e74dd75a5277d9b685b907ea2108e0e6d3995
damian-ngau <damianngau@gmail.com>
2025-03-14 01:36:23 +1100

    railway deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,8 @@ const App = () => {
   const fetchWeather = async () => {
     try {
       const response = await axios.get(
-        `http://localhost:5000/weather?city=${city}`
+        // `http://localhost:5000/weather?city=${city}` //use this for local deployment
+        `https://sunsafe-website.up.railway.app/weather?city=${city}` // use this for railway deployment
       );
       setWeather(response.data);
       setError(null);
